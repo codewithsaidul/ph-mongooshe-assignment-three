0ee6b6c73644d746a6172b21814c8ecaa269fdb5
codewithsaidul <codewithsaidul@gmail.com>
2025-06-19 21:21:01 +0600

    book schema added

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from "express";
+import router from "./router";
 
 
 
@@ -6,6 +7,8 @@ import express, { Application, Request, Response } from "express";
 const app: Application = express();
 app.use(express.json())
 
+
+app.use(router)
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Assignment-3");
---
+++ src/model/book.model.ts
@@ -0,0 +1,35 @@
+import { model, Schema } from "mongoose";
+import { IBook } from "../interface/book.interface";
+
+const bookSchema = new Schema<IBook>({
+  title: { type: String, required: [true, "title is required"] },
+  author: { type: String, required: [true, "author is required"] },
+  genre: {
+    type: String,
+    uppercase: true,
+    enum: {
+      values: [
+        "FICTION",
+        "NON_FICTION",
+        "SCIENCE",
+        "HISTORY",
+        "BIOGRAPHY",
+        "FANTASY",
+      ],
+      message: "genre is not valid, got {values} genre"
+    },
+    
+  },
+  description: String,
+  copies: [Number, "book copies are required"],
+  available: {
+    type: Boolean,
+    default: true
+  }
+});
+
+
+
+const Book = model("Book", bookSchema);
+
+export default Book
